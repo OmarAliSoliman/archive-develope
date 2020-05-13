7fe789c481014fd4d26e3a394964257c9e71c037
omar <omarasmar9955@gmail.com>
2020-05-13 12:40:15 +0200

    first-commit

---
+++ js/main.js
@@ -0,0 +1,37 @@
+$(document).ready(function(){
+
+
+  var swiper = new Swiper('.swiper-container', {
+    slidesPerView: 1,
+    spaceBetween: 10,
+    // init: false,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    breakpoints: {
+      640: {
+        slidesPerView: 2,
+        spaceBetween: 0,
+      },
+      768: {
+        slidesPerView: 3,
+        spaceBetween: 0,
+      },
+      1024: {
+        slidesPerView: 4,
+        spaceBetween: 0,
+      },
+      1025: {
+        slidesPerView: 5,
+        spaceBetween: 0,
+      },
+    }
+  });
+
+
+})
